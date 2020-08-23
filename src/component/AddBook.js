1a6de11bd6e3869ae01375c338755b7ee4e5a1ff
parth <parthkanani8490@gmail.com>
2020-08-23 16:05:29 +0530

    create state and update the state by its take value at front side

--- src/component/AddBook.js
+++ src/component/AddBook.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { useQuery } from '@apollo/client';
 import {getauthorquery} from '../queries/query';
 
 
 
 
-
-
 function AddBook() {
     const { loading, data } = useQuery(getauthorquery); //using usequryhooks we get loading and data from bookquery
+    const [name ,setname] =useState("");
+    const [authorname ,setauthorname] =useState("");
+    const [genre ,setgenre] =useState("");
+    
+
+
     if (loading) return <h2>Loading.....</h2>
 
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={(e)=> {
+            e.preventDefault();
+        }}>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text" />
+                <input type="text" onChange={(e)=> setname(e.target.value)} />
             </div>
             <div className="field">
                 <label>Genre:</label>
-                <input type="text" />
+                <input type="text" onChange={(e=>setgenre(e.target.value))} />
             </div>
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select onChange={(e)=>setauthorname(e.target.value)}>
                     <option>Select author</option>
                     {/* add authors list in option from db */}
                     {data.authors.map((author) =>
--- src/component/BookList.js
+++ src/component/BookList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import {getbookquery} from '../queries/query';
 
 
