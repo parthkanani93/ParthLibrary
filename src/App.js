8a593778b9d41d4260fbea9c85fec8415df03326
parth <parthkanani8490@gmail.com>
2020-08-23 11:16:12 +0530

    apollo client setup in app.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import BookList from './component/BookList';
+import BookList from './component/BookList'; //import component
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+//create apollo client
+
+const client = new ApolloClient({
+  uri: "http://localhost:3003/graphql"
+
+})
+
+
 
 function App() {
   return (
-    <div id="main">
-      <h1>Parth Website</h1>
-      <BookList />
-      
-    </div>
+    <ApolloProvider client={client} >
+      <div id="main">
+        <h1>Parth Website</h1>
+        <BookList />
+
+      </div>
+    </ApolloProvider>
+
   );
 }
 
